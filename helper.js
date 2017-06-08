c49f461c0bd9a8c809a0912bcadfc5f66dd8f4ab
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-06-08 19:50:12 -0400

    Cornwall Centre 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -102,7 +102,7 @@ function init() {
 }
 
 function show_content(){
-    $(".yield").fadeIn();
+    $(".yield").css({visibility: "visible"});
     $(".modal-backdrop").remove();
     
     var header_stores = getStoresList();
