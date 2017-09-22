16cabc90baa38350195ea773ac6bdc833088c089
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2017-09-22 12:19:57 -0400

    Cornwall Centre 1.1  /helper.js

--- helper.js
+++ helper.js
@@ -67,7 +67,7 @@ function init() {
     $('#subForm').submit(function (e) {
         data = {"g-recaptcha-response": $('#subForm .g-recaptcha-response').val()}
         $.post("https://cornwall.mallmaverick.com/verify_captcha", data, function(response){
-            if(response == false){
+            if(response.success == false){
                 grecaptcha.reset(widgetId1);
                 alert("Please revalidate captcha");
                 return false;
